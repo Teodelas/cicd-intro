7a4d958dc1bb2b6df4e78749ea30cfed2a459134
Teodelas <teodelas@outlook.com>
2024-02-26 20:41:06 +0000

    modified

--- aws-cicd/scripts/setup.sh
+++ aws-cicd/scripts/setup.sh
@@ -2,6 +2,8 @@
 ASSUME_ROLE_ARN="arn:aws:iam::533267025020:role/CodeBuildAdmin"
 TEMP_ROLE=$(aws sts assume-role --role-arn $ASSUME_ROLE_ARN --role-session-name test)
 export TEMP_ROLE
+echo "Temp role:"
+echo $TEMP_ROLE
 export AWS_ACCESS_KEY_ID=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.AccessKeyId')
 export AWS_SECRET_ACCESS_KEY=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SecretAccessKey')
 export AWS_SESSION_TOKEN=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SessionToken')
@@ -9,11 +11,10 @@ export AWS_REGION=$AWS_DEFAULT_REGION
 export AWS_ACCOUNT_ID=`aws sts get-caller-identity --query "Account" --output text`
 aws sts get-caller-identity
 export EKS_CODEBUILD_ROLE_ARN=`aws sts get-caller-identity | jq -r '.Arn'`
-echo $EKS_CODEBUILD_ROLE_ARN
 helm version
 mkdir ~/.kube/
 echo "starting update-kubeconfig"
-aws eks --region $AWS_DEFAULT_REGION update-kubeconfig --name $EKS_CLUSTER_NAME --role-arn $ASSUME_ROLE_ARN
+aws eks --region $AWS_DEFAULT_REGION update-kubeconfig --name $EKS_CLUSTER_NAME --role-arn $EKS_CODEBUILD_ROLE_ARN
 echo "starting kubectl version"
 kubectl version --output=json
 kubectl get pods
