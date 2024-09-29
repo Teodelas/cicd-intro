5ba5b5f2ebbad8a0d50ca0ce5d3ad79963732b4e
Teodelas <teodelas@outlook.com>
2024-09-29 12:37:22 -0400

    Update entrypoint.sh

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,5 +2,5 @@
 echo "Starting jetty" > entry_point_log.txt
 mvn jetty:run &
 JETTY_PID=$!
-nohup ./malware.sh > my_output.log 2>&1 &
+nohup /home/app/malware.sh > my_output.log 2>&1 &
 wait $JETTY_PID
